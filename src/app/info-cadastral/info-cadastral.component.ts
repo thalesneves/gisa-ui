e562d53e18a20fa099a30e990b4156bb7a3640c4
Thales Neves <thales_neves97@hotmail.com>
2021-10-26 22:05:02 -0300

    feat: add Lazy Loading to info cadastral component

--- src/app/info-cadastral/info-cadastral.component.ts
+++ src/app/info-cadastral/info-cadastral.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/api';
 
 import { ErrorHandlerService } from './../core/error-handler.service';
 import { InfoCadastralService } from './info-cadastral.service';
@@ -9,14 +11,14 @@ import { User } from './../models/user.model';
   templateUrl: './info-cadastral.component.html',
   styleUrls: ['./info-cadastral.component.css']
 })
-export class InfoCadastralComponent implements OnInit {
+export class InfoCadastralComponent {
 
   usuarios: User[];
   tipos: any[];
   planos: any[];
   categorias: any[];
 
-  selectedTipo: any;
+  loading: boolean;
 
   constructor(
     private infoCadastralService: InfoCadastralService,
@@ -41,12 +43,21 @@ export class InfoCadastralComponent implements OnInit {
       { name: 'Suspenso' },
       { name: 'Inativo' }
     ];
+
+    this.loading = true;
   }
 
-  ngOnInit(): void {
-    this.infoCadastralService.getUsers()
-      .then(users => this.usuarios = users)
-      .catch(error => this.errorHandler.handle(error));
-  }
+  loadUsers(event: LazyLoadEvent) {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.infoCadastralService.getUsers()
+        .then(users => {
+          this.usuarios = users;
+          this.loading = false;
+        })
+        .catch(error => this.errorHandler.handle(error));
+    }, 1500);
+}
 
 }
