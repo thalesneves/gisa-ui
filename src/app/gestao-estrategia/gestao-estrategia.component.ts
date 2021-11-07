e2ac27a42f8c7ed3afd32180f7a1511f13a5c8a9
Thales Neves <thales_neves97@hotmail.com>
2021-11-07 12:15:03 -0300

    feat: updating the management and strategy component

--- src/app/gestao-estrategia/gestao-estrategia.component.ts
+++ src/app/gestao-estrategia/gestao-estrategia.component.ts
@@ -7,25 +7,111 @@ import { Component } from '@angular/core';
 })
 export class GestaoEstrategiaComponent {
 
-  basicData: any;
+  basicDataBar: any;
+  basicDataLine: any;
+  basicDataDoughnutChart: any;
+  basicDataBarHorizontal: any;
 
-  basicOptions: any;
+  horizontalOptions: any;
 
   ngOnInit() {
-    this.basicData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-            {
-                label: 'My First dataset',
-                backgroundColor: '#42A5F5',
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: 'My Second dataset',
-                backgroundColor: '#FFA726',
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
+    this.basicDataBar = {
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+      datasets: [
+        {
+          label: 'Realizadas',
+          backgroundColor: '#42A5F5',
+          data: [365, 259, 180, 281, 256]
+        },
+        {
+          label: 'Não realizadas',
+          backgroundColor: '#FFA726',
+          data: [12, 8, 15, 9, 3]
+        }
+      ]
+    };
+
+    this.basicDataLine = {
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+      datasets: [
+        {
+          label: 'Aumento de clientes',
+          data: [280, 320, 260, 140, 210],
+          fill: false,
+          backgroundColor: '#20DB8C',
+          borderColor: '#20DB8C',
+          tension: .4
+        },
+        {
+          label: 'Perdas de clientes',
+          data: [20, 14, 10, 50, 25],
+          backgroundColor: '#e32636',
+          borderColor: '#e32636',
+          tension: .4
+        }
+      ]
+    };
+
+    this.basicDataDoughnutChart = {
+      labels: ['Adimplentes','Inadimplentes'],
+      datasets: [
+        {
+          data: [400, 50],
+          backgroundColor: [
+            "#FF6384",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#FFCE56"
+          ]
+        }
+      ]
+    };
+
+    this.basicDataBarHorizontal = {
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+      datasets: [
+        {
+          label: 'Receitas',
+          backgroundColor: '#1EE07D',
+          data: [8, 10, 7, 5, 9]
+        },
+        {
+          label: 'Despesas',
+          backgroundColor: '#FD1556',
+          data: [1, 2, 1, 3, 2]
+        }
+      ]
+    };
+
+    this.horizontalOptions = {
+      indexAxis: 'y',
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        }
+      }
     };
   }
 
